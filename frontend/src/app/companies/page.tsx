f41d9329af294c74b30f603d0d04bb9c6c915afe
anrokk <andreasrokk@gmail.com>
2025-06-26 02:32:08 +0300

    dockerizing db, frontend & backend

--- frontend/src/app/companies/page.tsx
+++ frontend/src/app/companies/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Company } from "@/types/api";
 import { getCompanies } from "@/services/apiService";
 import CompanyCard from "@/components/CompanyCard";
