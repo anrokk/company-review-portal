4acc3bff7a2d7277f70ce6e715d70653a33c064a
anrokk <andreasrokk@gmail.com>
2025-06-25 13:21:47 +0300

    navbar auth

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,19 @@
+'use client';
+
 import Link from 'next/link';
+import { useAuth } from '@/context/AuthContext';
+import { useRouter } from 'next/navigation';
 
 const Navbar = () => {
+
+  const { isAuthenticated, user, logout } = useAuth();
+  const router = useRouter();
+
+  const handleLogout = () => {
+    logout();
+    router.push('/login');
+  }
+
   return (
     <header className="sticky top-0 z-50 bg-black/50 backdrop-blur-lg border-b border-neutral-800/50">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
@@ -8,12 +21,25 @@ const Navbar = () => {
           Intereview
         </Link>
         <div className="space-x-4 flex items-center">
-          <Link href="/login" className="px-4 py-2 rounded-md text-sm font-medium text-white hover:bg-neutral-900 transition-colors">
-            Login
-          </Link>
-          <Link href="/register" className="px-4 py-2 rounded-md text-sm font-medium text-black bg-white hover:bg-gray-200 transition-all duration-300">
-            Register
-          </Link>
+          {isAuthenticated ? (
+            <>
+              <span className='text-gray-400 hidden sm:block'>Logged in as {user?.username}</span>
+              <button
+                onClick={handleLogout}
+                className='px-4 py-2 rounded-md text-sm font-medium text-white hover:bg-neutral-900 cursor-pointer transition-all duration-300'>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link href="/login" className="px-4 py-2 rounded-md text-sm font-medium text-white hover:bg-neutral-900 transition-all duration-300">
+                Login
+              </Link>
+              <Link href="/register" className="px-4 py-2 rounded-md text-sm font-medium text-black bg-white hover:bg-gray-200 transition-all duration-300">
+                Register
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </header>
